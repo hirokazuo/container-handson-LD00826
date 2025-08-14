14ac47556ff98a3f476db2779b9391f162db397d
hontap <51691422+hontap@users.noreply.github.com>
2025-08-14 16:24:18 +0900

    Update setup_k8s_worker1_crio.sh

--- scripts/setup_k8s_worker1_crio.sh
+++ scripts/setup_k8s_worker1_crio.sh
@@ -1,41 +1,53 @@
 #!/bin/bash
 
 ####################################################################################
-# NetApp LOD: 
-# Setup k8s worker node on gpu01
+# Update: 2025-08-13
+# NetApp LOD: LD00826
+# Setup k8s master node on gpu01
 # Host OS: Ubuntu 20.04.4 LTS
-# CRI: CRI-O
-# Kubernetes version: v1.30
+# CRI: CRI-O v1.33
+# Kubernetes version: v1.33
 # note: 
 ####################################################################################
+
+# --------------------------------------------------------------------------------
+# /root/.docker/config.jsonの更新
+# 自分のクレデンシャル情報で作成したconfig.jsonに置き換わっていることを確認してください
+# --------------------------------------------------------------------------------
 
 KUBERNETES_VERSION=v1.33
 CRIO_VERSION=v1.33
 
-####################################################################################
-# Container runtimes: install CRI-O, kubelet, kubeadm and kubectl
-# https://github.com/cri-o/packaging/blob/main/README.md#distributions-using-deb-packages
-####################################################################################
+# --------------------------------------------------------------------------------
+# コンテナランタイム CRI-O インストール
+# kubelet, kubeadm and kubectl インストール
+# https://github.com/cri-o/packaging/blob/main/README.md
+# https://cri-o.io/
+# distributions-using-deb-packages
+# --------------------------------------------------------------------------------
 
-# Install the dependencies for adding repositories
+# リポジトリを追加するための依存関係を設定
 apt-get update
 apt-get install -y software-properties-common curl
 
-# Add the Kubernetes repository
+# リポジトリを追加するための依存関係を設定
+apt-get update
+apt-get install -y software-properties-common curl
+
+# Ubuntu 22.04より古いリリースでは、/etc/apt/keyringsフォルダーはデフォルトでは存在しないため、curlコマンドの前に作成
 mkdir -p -m 755 /etc/apt/keyrings
+
+# Kubernetes リポジトリを追加
 curl -fsSL https://pkgs.k8s.io/core:/stable:/$KUBERNETES_VERSION/deb/Release.key |
     gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
-
 echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/$KUBERNETES_VERSION/deb/ /" |
     tee /etc/apt/sources.list.d/kubernetes.list
 
-# Add the CRI-O repository
-curl -fsSL https://pkgs.k8s.io/addons:/cri-o:/stable:/$CRIO_VERSION/deb/Release.key |
+# CRI-O リポジトリを追加
+curl -fsSL https://download.opensuse.org/repositories/isv:/cri-o:/stable:/$CRIO_VERSION/deb/Release.key |
     gpg --dearmor -o /etc/apt/keyrings/cri-o-apt-keyring.gpg
-
-echo "deb [signed-by=/etc/apt/keyrings/cri-o-apt-keyring.gpg] https://pkgs.k8s.io/addons:/cri-o:/stable:/$CRIO_VERSION/deb/ /" |
+echo "deb [signed-by=/etc/apt/keyrings/cri-o-apt-keyring.gpg] https://download.opensuse.org/repositories/isv:/cri-o:/stable:/$CRIO_VERSION/deb/ /" |
     tee /etc/apt/sources.list.d/cri-o.list
-
 
 # Install the packages
 apt-get update
@@ -44,9 +56,9 @@ apt-get install -y cri-o kubelet kubeadm kubectl
 # Start CRI-O
 systemctl start crio.service
 
-#######################################################
-# Setup kubernetes worker node
-#######################################################
+# --------------------------------------------------------------------------------
+# kubernetes ワーカーノード　セットアップ
+# --------------------------------------------------------------------------------
 
 # Bootstrap a cluster
 swapoff -a
@@ -54,8 +66,6 @@ modprobe br_netfilter
 sysctl -w net.ipv4.ip_forward=1
 sysctl -w fs.inotify.max_user_instances=2280
 sysctl -w fs.inotify.max_user_watches=1255360
-
-
 
 
 echo -e "\n\n\n続いて、kubernates masterノードをセットアップ時に確認したkubeadm joinコマンドを使ってこのホストをkubernatesクラスタに参加させます\n"
